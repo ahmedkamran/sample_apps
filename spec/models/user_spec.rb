ed99888d3d0e31c10201d16cdc849fea553a5d52
ahmedkamran <kamranseacom@gmail.com>
2013-07-18 00:41:42 +0530

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,29 +12,40 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
-   it { should respond_to(:password) }
+  it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
   it { should be_valid }
-  
-   describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-   describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-   describe "when name is too long" do
+
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-   describe "when email format is invalid" do
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -54,14 +65,7 @@ describe User do
       end
     end
   end
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
 
-    it { should_not be_valid }
-  end
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -71,19 +75,22 @@ describe User do
 
     it { should_not be_valid }
   end
-   describe "when password is not present" do
+
+  describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
+                     password: " ", password_confirmation: " ")
     end
+
     it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
+    it { should_not be_valid } 
   end
-   describe "with a password that's too short" do
+
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -102,5 +109,9 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
